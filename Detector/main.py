e7e0e1c39cfc2f06e37d083a4db2b59fb01c7d18
Dmitry-tol <tolstikovdima321@gmail.com>
2025-01-13 19:01:06 +0500

    Update main.py

--- Detector/main.py
+++ Detector/main.py
@@ -1,55 +1,86 @@
 import cv2
+from ultralytics import YOLO
+
 
 class Detector:
-    MODEL = cv2.CascadeClassifier('Detector/model/model.xml')
-    MODEL_PARAM = {'scaleFactor': 1.1, 'minNeighbors': 5}
-
-    def __init__(self, img_source):
-        self.__img_source = img_source
+    def __init__(self, img_path, model_path='model/best (2).pt'):
+        self.__img_path = img_path
+        self.model = YOLO(model_path)
+        self.coords = []
 
     @staticmethod
-    def show_img(img) -> None:
-        cv2.imshow('Detected Image', img)
+    def show_img(img, window_name='Detected Image') -> None:
+        cv2.imshow(window_name, img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     @staticmethod
-    def draw_box(img, x1, y1, x2, y2):
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    def draw_box(img, x1, y1, x2, y2, color=(0, 255, 0), thickness=2):
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, thickness)
 
     def open_img(self):
-        if isinstance(self.__img_source, str):
-            car_img = cv2.imread(self.__img_source)
-            car_img = cv2.cvtColor(car_img, cv2.COLOR_BGR2RGB)
-        else:
-            car_img = self.__img_source
+        car_img = cv2.imread(self.__img_path)
+        if car_img is None:
+            raise FileNotFoundError(f"Не удалось загрузить изображение по пути: {self.__img_path}")
+        car_img = cv2.cvtColor(car_img, cv2.COLOR_BGR2RGB)
         return car_img
 
     def __plate_extract(self):
         car_img = self.open_img()
-        rects = self.MODEL.detectMultiScale(car_img,
-                                            scaleFactor=self.MODEL_PARAM['scaleFactor'],
-                                            minNeighbors=self.MODEL_PARAM['minNeighbors'])
 
-        for x, y, w, h in rects:
-            x1 = x
-            x2 = x + w
-            y1 = y
-            y2 = y + h
-            # result = car_img[y1:y2, x1:x2]
-            self.draw_box(car_img, x1, y1, x2, y2)
-            # self.show_img(car_img)  # Показать изображение с прямоугольником
-        return car_img, rects
+        results = self.model(car_img)
+
+        def process_boxes(img, result):
+
+            boxes = result.boxes.xyxy.cpu().numpy()
+
+            self.coords = result.boxes.xyxy.cpu().numpy()
+            confidences = result.boxes.conf.cpu().numpy()
+            class_ids = result.boxes.cls.cpu().numpy()
+
+            extracted_plates = []
+
+            for box, confidence, class_id in zip(boxes, confidences, class_ids):
+                x1, y1, x2, y2 = map(int, box)
+
+                if class_id == 0:
+                    plate_img = img[y1:y2, x1:x2]
+                    self.draw_box(car_img, x1, y1, x2, y2)
+                    extracted_plates.append(plate_img)
+                    break
+            return extracted_plates
+
+        extracted_plates = process_boxes(car_img, results[0])
+
+        self.show_img(car_img)
+        if len(extracted_plates) > 0:
+            return extracted_plates[0]
+        else:
+            return None
 
     def __resize_img(self, img, scale_percent: int):
+        if img is None: return None
+
         height = int(img.shape[0] * scale_percent / 100)
         width = int(img.shape[1] * scale_percent / 100)
 
         resized_img = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-
         return resized_img
 
     def get_license_plate(self):
-        extract_img, rects = self.__plate_extract()
-        # extract_img = self.__resize_img(extract_img, 150)
-        return extract_img, rects
+
+        extract_img = self.__plate_extract()
+
+        if extract_img is not None:
+            extract_img = self.__resize_img(extract_img, 150)
+            return extract_img
+        else:
+            return None
+detector = Detector('path img')
+license_plate_image = detector.get_license_plate()
+
+if license_plate_image is not None:
+    Detector.show_img(license_plate_image, 'Extracted License Plate')
+
+else:
+    print("Не удалось обнаружить номерной знак на изображении")
